7fab7105d041ac5e314efba8f7270ae98f241927
moon <msy9904@naver.com>
2025-11-18 11:41:03 +0900

    메뉴와 장바구니 이어두기

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import MenuList from "./pages/menu/MenuList";
 import CartPage from "./pages/CartPage";
-import OrderCompletePage from "./pages/OderPage";
+import OrderCompletePage from "./pages/OrderPage";
 
 
 function App() {
@@ -15,44 +15,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<MenuList />} />
         <Route path="/cart" element={<CartPage />} />
-        <Route path="/order-complete" element={<OrderCompletePage />} />
+        <Route path="/order-complete" element={<OrderCompletePage/>} />
       </Routes>
       <Footer/>
     </>
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { CartProvider } from "./context/CartContext";
-
-
-import CartPage from "./pages/CartPage";
-import OrderCompletePage from "./pages/OrderPage";
-import TestMenu from "./pages/TestMenu";
-import Footer from "./components/Footer";
-import RightSidebar from "./components/RightSidebar";
-
-
-function App() {
-  return (
-    <CartProvider>
-      <Router>
-        <div className="d-flex flex-column min-vh-100">
-         
-
-          <main className="container my-4 flex-grow-1">
-            <Routes>
-             
-              <Route path="/oder" element={<OrderCompletePage />} />
-               <Route path="/menu" element={<TestMenu />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route path="/order-complete" element={<OrderCompletePage />} />
-          
-            </Routes>
-          </main>
-           <RightSidebar/>
-          <Footer/>
-        </div>
-      </Router>
-    </CartProvider>
   );
 }
 
