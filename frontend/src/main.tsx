1864b159cb3f1cc3008eb8d2c444dc8cb657fd7d
moon <msy9904@naver.com>
2025-11-18 13:11:35 +0900

    백엔드 적용?

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
-import { CartProvider } from "./context/CartContext";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.tsx';
+import { CartProvider } from './context/CartContext.tsx';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <CartProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </CartProvider>
-  </React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <CartProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  </CartProvider>
 );
