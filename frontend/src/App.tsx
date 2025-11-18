6cb6ba512e6d553604aa9f8f19061843d95cc41d
murasakijyuutann <yuuka.hayase1030@gmail.com>
2025-11-18 11:45:44 +0900

    App.tsx and main.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,55 +1,27 @@
-import { Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import MenuList from "./pages/menu/MenuList";
-import CartPage from "./pages/CartPage";
-import OrderCompletePage from "./pages/OderPage";
-
-
-function App() {
-  return (
-  <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<MenuList />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/order-complete" element={<OrderCompletePage />} />
-      </Routes>
-      <Footer/>
-    </>
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
-
-
+import Home from "./pages/Home";
 import CartPage from "./pages/CartPage";
 import OrderCompletePage from "./pages/OrderPage";
 import TestMenu from "./pages/TestMenu";
 import Footer from "./components/Footer";
 import RightSidebar from "./components/RightSidebar";
 
-
 function App() {
   return (
     <CartProvider>
       <Router>
         <div className="d-flex flex-column min-vh-100">
-         
-
           <main className="container my-4 flex-grow-1">
             <Routes>
-             
-              <Route path="/oder" element={<OrderCompletePage />} />
-               <Route path="/menu" element={<TestMenu />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/menu" element={<TestMenu />} />
               <Route path="/cart" element={<CartPage />} />
               <Route path="/order-complete" element={<OrderCompletePage />} />
-          
             </Routes>
           </main>
-           <RightSidebar/>
-          <Footer/>
+          <RightSidebar />
+          <Footer />
         </div>
       </Router>
     </CartProvider>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,11 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App.tsx';
-import { CartProvider } from './context/CartContext.tsx';
-
-createRoot(document.getElementById('root')!).render(
-  <CartProvider>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </CartProvider>
-);
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { CartProvider } from "./context/CartContext";
-import "./App.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-   <CartProvider>
-      <App />
-    </CartProvider>
+    <App />
   </React.StrictMode>
 );
